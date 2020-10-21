c18f09277943a6801164e3da9cbaa8d7935a6020
Wyjbuss <wyjbuss@gmail.com>
2020-10-21 13:31:57 -0500

    started basic porfolio site using ejs, express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const ejs = require("ejs");
+
+const app = express();
+
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
+app.use(express.static('public'));
+app.set("view engine", 'ejs')
+
+const port = process.env.PORT || 3000
+
+app.get('/', function(req, res) {
+	res.render(`home.ejs`)
+});
+
+
+
+
+
+app.listen(port, function() {
+	console.log(`server running on port: ${port}`);
+});
